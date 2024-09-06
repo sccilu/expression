0d1220acb5af8de0c6ec7db0acee2dcd70ece105
sccilu <93533134+sccilu@users.noreply.github.com>
2024-09-06 12:49:12 +0800

    Initial commit

---
+++ data/README.dataset.txt
@@ -0,0 +1,6 @@
+# Expression > expression
+https://universe.roboflow.com/workshop-yg2yt/expression-abyan
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ yolo_train.py
@@ -0,0 +1,25 @@
+import torch
+from ultralytics import YOLO
+
+def train_model():
+
+    model = YOLO('yolov8n-p2.yaml').load('yolov8n.pt')
+    #model = YOLO('runs/detect/train/weights/last.pt')
+    # 进行模型训练
+    model.train(
+	#从头开始训练
+        data='data.yaml',
+        epochs=10,
+        imgsz=640,
+        device='mps',
+        workers = 4,
+        batch =4,
+    )
+
+    # 进行模型验证s
+    model.val()
+if __name__ == "__main__":
+    # 调用训练函数
+    train_model()
+
+    #  python yolo_train.py
