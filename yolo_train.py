6626f6c03e4940b895d99135d9c07352e13fa309
sccilu <93533134+sccilu@users.noreply.github.com>
2024-09-07 20:51:54 +0800

    Update yolo_train.py

--- yolo_train.py
+++ yolo_train.py
@@ -1,26 +1,24 @@
 import torch
 from ultralytics import YOLO
 import os
-import wandb
-os.environ['WANDB_DISABLE_LOGGING'] = '1'
-
 
 def train_model():
+    # 设置CUDA环境
+    device = torch.device("cuda")
+    print(f"Using GPU: {device}")
 
     model = YOLO('yolov8n-p2.yaml').load('yolov8n.pt')
-    #model = YOLO('runs/detect/train/weights/last.pt')
     # 进行模型训练
     model.train(
-	#从头开始训练
+        # 从头开始训练
         data='data.yaml',
         epochs=50,
         imgsz=640,
-        device='cpu',
+        device=device,
         workers = 4,
         batch =64,
     )
-
-    # 进行模型验证s
+    # 进行模型验证
     model.val()
 if __name__ == "__main__":
     # 调用训练函数
